10f1c83a0d3f249cb61a1166b56a8d94deeaa322
ConwayC1987 <conwayciaran@gmail.com>
2023-04-01 21:10:52 +0000

    Add rules at the start of the game

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ for suit in ['\u2663', '\u2660', '\u2665', '\u2666']:
         # Joins the rank and card suit using the unicode for visual appeal.
         # Creates a full deck index 0 to 51.
         deck.append(rank+suit)
-# For loop to shuffle the deck.
+# For loop to shuffle the deck using the index.
 for x in range(len(deck)):
     card1 = deck[x]
     r = random.randint(0, len(deck)-1)
@@ -16,10 +16,16 @@ for x in range(len(deck)):
     deck[x] = card2
     deck[r] = card1
 
-for card in deck:
-    print(card, end=' ')
+#for card in deck:
+    #print(card, end=' ')
 # Define a player name
 print("Welcome to Blackjack!")
+print("Aim of JackJack is to get 21 or as close to as possible.")
+print("Jacks, kings and queens are worth 10. Ace can be either 1 or 11.")
+print("You get 2 cards face up and the dealer will recieve 1 card face down.")
+print("You have a choice to hit or stand until you or the dealer goes bust.")
+print("You will start with 100 chips and can bet each hand.")
+print("You will be playing against the computer.")
 
 name = input("What is your name? ")
 
