72bd9c19a739efb49a73f94a6856391ff9759111
ConwayC1987 <conwayciaran@gmail.com>
2023-04-01 20:49:20 +0000

    Add comments to the make deck and shuffle cards randomly and debug and test

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import random
 # Create a deck of cards
 deck = []
-# Deck idea from https://www.youtube.com/watch?v=jHjsG2lhUSI
-for suit in ['\u2663', '\u2660', '\u2665', '\u2666']:
+# Deck idea from https://www.youtube.com/watch?v=
+# Unicode characters used for the suits on the cards.
+for suit in ['\u2663', '\u2660', '\u2665', '\u2666']: 
     for rank in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
+        # Joins the rank and card suit using the unicode for visual appeal.
+        # Creates a full deck index 0 to 51.
         deck.append(rank+suit)
-
+# For loop to shuffle the deck.
 for x in range(len(deck)):
     card1 = deck[x]
     r = random.randint(0, len(deck)-1)
@@ -24,6 +27,3 @@ chips = 100
 
 winS = 0
 losseS = 0
-
-
-
