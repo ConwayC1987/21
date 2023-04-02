33b28b3eba2a8b1c54b026e9ce8ec15365442aa7
ConwayC1987 <conwayciaran@gmail.com>
2023-04-02 21:26:07 +0000

    Add some colour to the menu

--- deck_Cards.py
+++ deck_Cards.py
@@ -1,14 +1,28 @@
-deck = []
-for suit in ['\u2663', '\u2660', '\u2665', '\u2666']:
-    if suit in ['\u2663', '\u2660']:  # Clubs and Spades are black
-        color = '\033[30m'  # set color to black
-    else:  # Hearts and Diamonds are red
-        color = '\033[31m'  # set color to red
-    for rank in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
-        # Joins the rank and card suit using the unicode for visual appeal.
-        # Creates a full deck index 0 to 51.
-        deck.append(color + rank + suit + '\033[0m')  # add color to the card
 
-# print the deck of cards
-for card in deck:
-    print(card)
+def print_cards(cardList):
+    for card in zip(*cardList):
+        print(' '.join(card))
+
+
+def print_card(*cards):
+    for line in zip(*cards):
+        print(' '.join(line))
+
+
+def card_visual(card):
+    suits = "Spades Diamonds Hearts Clubs".split()
+    suit_symbols = ['♠','♦','♥','♣']
+    suit_pairs = dict(zip(suits, suit_symbols))
+
+    v = card.value
+    s = suit_pairs 
+
+    visual = [
+        '╔══════╗',
+       f'║{v:<3} ║',
+       f'║      ║',
+       f'║{s:>3} ║',
+        '╚══════╝'
+    ]
+
+    return visual
--- run.py
+++ run.py
@@ -12,9 +12,7 @@ for suit in ['\u2663', '\u2660', '\u2665', '\u2666']:
     for rank in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
         # Joins the rank and card suit using the unicode for visual appeal.
         # Creates a full deck index 0 to 51.
-        deck.append(color + rank + suit + '\033[0m')  # add color to the 
-
-   
+        deck.append(color + rank + suit + '\033[0m')  # add color to the cards.
 # For loop to shuffle the deck using the index.
 for x in range(len(deck)):
     card1 = deck[x]
@@ -23,8 +21,8 @@ for x in range(len(deck)):
     deck[x] = card2
     deck[r] = card1
 
-for card in deck:
-    print(card, end=' ')
+#for card in deck:
+    #print(card, end=' ')
 
 
 def clear_terminal():
@@ -35,22 +33,23 @@ def clear_terminal():
 
 
 # Define a player name
-name = input("What is your name? ")
-print("Welcome to \u001b[30mBlack\u001b[31mJack!")
+print("\u001b[1mWelcome to \u001b[30mBlack\u001b[31mJack!")
+name = input("\u001b[32mWhat is your name? ")
+
 # Create and game option to view the game
 while True:
     try:
-        print("Please read the rules first")
-        print("\nPress 2 to read the rules or 1 to play")
+        print("\u001b[42m\u001b[37;1mPlease read the rules first")
+        print("\nPress 2 to read the rules or 1 to play\u001b[0m")
         Options = int(input("\n1.Play Game\n2.Rules\noption"))
         if Options == 1:
             PlayGame = True
             break
 
-        elif Options == 2:
+        if Options == 2:
             clear_terminal()
             print("""
-            The rules are:
+            \u001b[41;1mThe rules are:\u001b[0m
             1.Aim of JackJack is to get 21 or as close to as possible. \n 
             2.Jacks, kings and queens are worth 10. Ace can be either 1 or 11.\n
             3.You get 2 cards face up and the dealer will recieve 1 card face down. \n
@@ -59,3 +58,5 @@ while True:
             6.You will be playing against the computer.""")
     except ValueError:
         print("Invalid input. Please enter a number.")
+
+# Add a class to make the cards
